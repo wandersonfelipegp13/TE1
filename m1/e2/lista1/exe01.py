6aa39ba5f094362d13a8e5b725c6ea4b53b5e698
Wanderson <wandersonfelipeplus13@gmail.com>
2021-05-14 12:35:20 -0300

    Commit inicial.

---
+++ m1/e2/lista1/exe01.py
@@ -0,0 +1,8 @@
+"""
+1. Faça um programa que leia um caractere ‘F’ ou ‘C’, que indica se o próximo valor
+corresponde à temperatura em Fahrenheit ou Celsius. Em seguida, o programa deve ler o
+valor da temperatura e então imprimir o valor correspondente à temperatura na outra
+unidade de medida. Observação: (C = 5/9 × (F − 32)).
+"""
+
+
