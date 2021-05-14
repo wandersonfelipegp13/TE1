08b568dd30f72700b885d265f926db1f34ceede9
Wanderson <wandersonfelipeplus13@gmail.com>
2021-05-14 13:21:38 -0300

    Exercicio 1 feito.

--- m1/e2/lista1/exe01.py
+++ m1/e2/lista1/exe01.py
@@ -5,4 +5,9 @@ valor da temperatura e então imprimir o valor correspondente à temperatura na 
 unidade de medida. Observação: (C = 5/9 × (F − 32)).
 """
 
-
+option = input("Conversor de temperaturas\n F - Fahrenheit\n C - Celsius\n$ ")
+temper = float(input("Temperatura: "))
+if (option == 'F'):
+    print(f"{temper} F = " + format(5 / 9 * (temper - 32), ".2f"), "C")
+else:
+    print(f"{temper} C = " + format((temper * 9/5) + 32, ".2f"), "F")
