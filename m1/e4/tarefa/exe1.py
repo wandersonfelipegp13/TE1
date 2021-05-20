7ec09a412854578ff26e60715f946100c86cb22c
Wanderson <wandersonfelipeplus13@gmail.com>
2021-05-19 22:22:26 -0300

    m1e4 tarefa - exe1

---
+++ m1/e4/tarefa/exe1.py
@@ -0,0 +1,38 @@
+"""
+1. Escreva um programa que calcule o preço a pagar pelo fornecimento de energia
+elétrica. Pergunte a quantidade de kWh consumida e o tipo de instalação: R para
+residências, I para indústrias e C para comércios. Calcule o preço a pagar de acordo com
+a tabela a seguir.
++---------------------------------------+
+|   Preço por tipo e faixa de consumo   |
++---------------------------------------+
+| Tipo        | Faixa (kWh)   | Preço   |
++=======================================+
+| Residencial | Até 500       | R$ 0,40 |
+|             | Acima de 500  | R$ 0,65 |
++---------------------------------------+
+| Comercial   | Até 1000      | R$ 0,55 |
+|             | Acima de 1000 | R$ 0,60 |
++---------------------------------------+
+| Industrial  | Até 5000      | R$ 0,55 |
+|             | Acima de 5000 | R$ 0,60 |
++---------------------------------------+
+"""
+
+csm = int(input("Consumo (kWh): "))
+typ = input(" [R] Residências\n [I] Indústrias \n [C] Comércios\nInstalação: ")
+if typ == 'R':
+    if csm > 500:
+        print("Preço = R$ 0,65")
+    else:
+        print("Preço = R$ 0,40")
+elif typ == 'I':
+    if csm > 5000:
+        print("Preço = R$ 0,60")
+    else:
+        print("Preço = R$ 0,55")
+else:
+    if csm > 1000:
+        print("Preço = R$ 0,60")
+    else:
+        print("Preço = R$ 0,55")
---
+++ slides/condicionais/exemplo001.py
@@ -0,0 +1,9 @@
+a = int(input())
+if a > 3:
+    if a < 7:
+        print("a")
+else:
+    if a > -10:
+        print("b")
+    else:
+        print("c")
