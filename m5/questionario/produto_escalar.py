26bc76d929b6d1a979aa1a00f49ad0448ff419f8
Wanderson <wandersonfelipeplus13@gmail.com>
2021-07-18 16:02:13 -0300

    m5e3 questionario - calculo ponderado funcionando

--- m5/questionario/produto_escalar.py
+++ m5/questionario/produto_escalar.py
@@ -1,5 +1,14 @@
 import numpy as np
-a = [1, -2, 3, 4]
-b = [2, 3, -2, 1]
-V = np.dot(a, b)
-print(V)
+# 1) Criar uma matriz X com 10 linhas e 2 colunas contendo números reais aleatórios
+print("Matriz X:")
+X = np.random.rand(10, 2)
+print(X)
+
+# 2) Criar um vetor W de tamanho 2 contendo números reais aleatórios.
+print("Vetor W:")
+W = np.random.rand(2)
+print(W)
+
+# 3) Apresente o resultado da produto escalar de cada linha da matriz X pelo vetor W
+for i in range(10):
+    print(f"({X[i][0]} * {W[0]}) + ({X[i][1]} * {W[1]}) = ", np.dot(X[i], W))
